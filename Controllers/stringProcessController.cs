176f8f61b5b8b7236b4bb4f0b62fbae7eb1281d9
DangAnh982001 <danganh982001@gmail.com>
2022-10-02 21:03:34 +0700

    DTA_2809

--- Controllers/stringProcessController.cs
+++ Controllers/stringProcessController.cs
@@ -1,17 +1,56 @@
-using DangThiAnhBTH.Models.Process;
 using Microsoft.AspNetCore.Mvc;
+using DangThiAnhBTH.Models.Process;
 
 namespace DangThiAnhBTH.Controllers
 {
-    public class stringProcessController : Controller
+    public class StringProcessController : Controller
     {
         stringProcess strPro = new stringProcess();
         [HttpPost]
-        public IActionResult Index(string strInput)
+        public IActionResult Index ()
         {
-            string thongBao = strPro.RemoveRemainingWhiteSpace(strInput);
-            ViewBag.result = thongBao;
             return View();
         }
+        [HttpPost]
+        public IActionResult Index (string strInput)
+        {
+            string result = strPro.RemoveRemainingWhiteSpace(strInput);
+            ViewBag.ketqua = result;
+            return View();
+        }
+        [HttpPost]
+         public IActionResult chuhoa (string strInput)
+        {
+            string result1 = strPro.LowerToUpper(strInput);
+            ViewBag.ketqua1 = result1;
+            return View();
+        }[HttpPost]
+         public IActionResult chuthuong (string strInput)
+         {
+             string result2 = strPro.UpperToLower(strInput);
+            ViewBag.ketqua2 = result2;
+             return View();
+         }
+         [HttpPost]
+         public IActionResult kytudauchuoi (string strInput)
+         {
+             string result3 = strPro.CapitalizeOneFirstCharacter(strInput);
+            ViewBag.ketqua3 = result3;
+             return View();
+         }
+         [HttpPost]
+         public IActionResult kytudaucactu (string strInput)
+         {
+             string result4 = strPro.CapitalizeFirstCharacter(strInput);
+            ViewBag.ketqua4 = result4;
+             return View();
+         }
+         [HttpPost]
+          public IActionResult Xoadaucactu (string strInput)
+         {
+             string result5 = strPro.RemoveRemainingWhiteSpace(strInput);
+            ViewBag.ketqua5 = result5;
+             return View();
+         }
     }
 }
